055a511e1ad06ad17366be4e42877680c127110e
Yury Shchamialiou <yury_shchamialiou@epam.com>
2023-03-29 14:20:02 +0200

    task-7

---
+++ authorization-service/src/handlers/basic-authorizer/basic-authorizer.js
@@ -0,0 +1,25 @@
+import {createSuccessResponse} from '../../utils/api'
+import {LOGIN} from '../../constants/constant'
+
+export const handler = (event, context, callback) => {
+
+    if (event.type !== 'TOKEN') {
+        callback('Unauthorized')
+    }
+
+    const encoded = event.authorizationToken.split('Basic ')[1].split('.')[1]
+
+    if (!encoded) {
+        callback(null, createSuccessResponse('user', 'Deny', event.methodArn))
+    }
+
+    const token = JSON.parse(Buffer.from(encoded, 'base64'))
+
+    const {user, password} = token
+
+    if (user === LOGIN && password === process.env['MrSmith']) {
+        callback(null, createSuccessResponse('user', 'Allow', event.methodArn))
+    } else {
+        callback(null, createSuccessResponse('user', 'Deny', event.methodArn))
+    }
+}
---
+++ authorization-service/src/utils/api.js
@@ -0,0 +1,13 @@
+export const createSuccessResponse = (principalId, effect, resource) => ({
+    principalId,
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: effect,
+          Resource: resource,
+        },
+      ],
+    },
+  })
